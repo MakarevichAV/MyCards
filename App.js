04637746340619f0c90ccd7e8f466e99de9a1b58
MakarevichAV <makarevich.a.v@yandex.ru>
2022-12-18 22:18:33 +0300

    -

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StatusBar, Text, View, Image } from 'react-native';
+import { Kit } from './components/Kit/Kit';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Kit title="Test" imageUrl="https://avatars.yandex.net/get-music-content/4382102/019f15b8.p.12747823/m1000x1000" />
+      <Kit title="Test" imageUrl="https://avatars.yandex.net/get-music-content/4382102/019f15b8.p.12747823/m1000x1000" />
+      <Kit title="Test" imageUrl="https://avatars.yandex.net/get-music-content/4382102/019f15b8.p.12747823/m1000x1000" />
+      <StatusBar theme='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
