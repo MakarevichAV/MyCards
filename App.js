d1e82aab16ae740e708f1b1b772bbc5d3b5bb8d0
MakarevichAV <makarevich.a.v@yandex.ru>
2023-03-11 18:59:10 +0300

    auth1 is done

--- App.js
+++ App.js
@@ -1,10 +1,71 @@
-import React from 'react';
-import { StyleSheet, RefreshControl, SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
-import { BottomBar } from './components/comon/BottomBar/BottomBar.jsx';
-import { TopBar } from './components/comon/TopBar/TopBar.jsx';
-import { HomePage } from './components/pages/HomePage/HomePage.jsx';
+import React, { ReactElement } from 'react'
+import { StyleSheet, StatusBar } from 'react-native';
 import { Navigation } from './components/pages/Navigator.jsx';
-import { SetsPage } from './components/pages/SetsPage/SetsPage.jsx';
+import Amplify from '@aws-amplify/core'
+import { Authenticator } from 'aws-amplify-react-native'
+import awsconfig from './src/aws-exports'
+import * as Keychain from 'react-native-keychain'
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true
+  }
+})
+
+// const MEMORY_KEY_PREFIX = '@MyStorage:'
+// let dataMemory = {}
+// class MyStorage {
+//   static syncPromise = null
+
+//   static setItem(key, value) {
+//     Keychain.setGenericPassword(MEMORY_KEY_PREFIX + key, value)
+//     dataMemory[key] = value
+//     return dataMemory[key]
+//   }
+
+//   static getItem(key) {
+//     return Object.prototype.hasOwnProperty.call(dataMemory, key) ? dataMemory[key] : undefined
+//   }
+
+//   static removeItem(key) {
+//     Keychain.resetGenericPassword()
+//     return delete dataMemory[key]
+//   }
+
+//   static clear() {
+//     dataMemory = {}
+//     return dataMemory
+//   }
+// }
+
+// Amplify.configure({
+//   ...awsconfig,
+//   Analytics: {
+//     disabled: false
+//   },
+//   storage: MyStorage
+// })
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password',
+    },
+  ],
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -23,15 +84,13 @@ const styles = StyleSheet.create({
 
 const App = () => {
   return (
-    <Navigation />
-    // <SafeAreaView style={styles.container}>
-    //   <ScrollView style={styles.scrollView}>
-    //     <TopBar />
-    //     <Navigation />
-    //   </ScrollView>
-    //   <BottomBar />
-    //   <StatusBar barStyle="light-content" theme='auto' />
-    // </SafeAreaView>
+    <>
+      < Authenticator
+        usernameAttributes="email"
+        signUpConfig={signUpConfig}
+      />
+      {/* <Navigation /> */}
+    </>
   );
 }
 
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mycards7a3cc0ab": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
