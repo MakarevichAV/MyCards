75765b45bff5c6784c00b5b34166e84a674e8717
MakarevichAV <makarevich.a.v@yandex.ru>
2023-03-11 21:33:14 +0300

    -

--- App.js
+++ App.js
@@ -6,46 +6,46 @@ import { Authenticator } from 'aws-amplify-react-native'
 import awsconfig from './src/aws-exports'
 import * as Keychain from 'react-native-keychain'
 
-Amplify.configure({
-  ...awsconfig,
-  Analytics: {
-    disabled: true
-  }
-})
-
-// const MEMORY_KEY_PREFIX = '@MyStorage:'
-// let dataMemory = {}
-// class MyStorage {
-//   static syncPromise = null
-
-//   static setItem(key, value) {
-//     Keychain.setGenericPassword(MEMORY_KEY_PREFIX + key, value)
-//     dataMemory[key] = value
-//     return dataMemory[key]
-//   }
-
-//   static getItem(key) {
-//     return Object.prototype.hasOwnProperty.call(dataMemory, key) ? dataMemory[key] : undefined
-//   }
-
-//   static removeItem(key) {
-//     Keychain.resetGenericPassword()
-//     return delete dataMemory[key]
-//   }
-
-//   static clear() {
-//     dataMemory = {}
-//     return dataMemory
-//   }
-// }
-
 // Amplify.configure({
 //   ...awsconfig,
 //   Analytics: {
-//     disabled: false
-//   },
-//   storage: MyStorage
+//     disabled: true
+//   }
 // })
+
+const MEMORY_KEY_PREFIX = '@MyStorage:'
+let dataMemory = {}
+class MyStorage {
+  static syncPromise = null
+
+  static setItem(key, value) {
+    Keychain.setGenericPassword(MEMORY_KEY_PREFIX + key, value)
+    dataMemory[key] = value
+    return dataMemory[key]
+  }
+
+  static getItem(key) {
+    return Object.prototype.hasOwnProperty.call(dataMemory, key) ? dataMemory[key] : undefined
+  }
+
+  static removeItem(key) {
+    Keychain.resetGenericPassword()
+    return delete dataMemory[key]
+  }
+
+  static clear() {
+    dataMemory = {}
+    return dataMemory
+  }
+}
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: false
+  },
+  // storage: MyStorage
+})
 
 const signUpConfig = {
   hideAllDefaults: true,
@@ -85,11 +85,11 @@ const styles = StyleSheet.create({
 const App = () => {
   return (
     <>
-      < Authenticator
+      {/* < Authenticator
         usernameAttributes="email"
         signUpConfig={signUpConfig}
-      />
-      {/* <Navigation /> */}
+      /> */}
+      <Navigation />
     </>
   );
 }
